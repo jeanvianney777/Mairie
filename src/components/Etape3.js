917392fb0b9ec49bb62e3d8630cbc8bb63484e83
Jean Marc Vianney Koffi <90218215+jeanvianney777@users.noreply.github.com>
2022-03-15 22:23:13 +0000

    Update Etape3.js

--- src/components/Etape3.js
+++ src/components/Etape3.js
@@ -4,6 +4,49 @@ const Etape3 = () => {
   return (
     <div>
       <h1 class="titreEtape">Informations de connexion:</h1>
+      <form action="">
+        <div class="fistinform">
+          <div class="form">
+            <input
+              type="text"
+              id="login"
+              name="login"
+              class="form__input"
+              autoComplete="off"
+              placeholder=" "
+            />
+            <label for="login" class="form__label">
+              Identifiant:
+            </label>
+          </div>
+          <div class="form">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              class="form__input"
+              autoComplete="off"
+              placeholder=" "
+            />
+            <label for="password" class="form__label">
+              Mot de passe:
+            </label>
+          </div>
+          <div class="form">
+            <input
+              type="password"
+              id="confpassword"
+              name="confpassword"
+              class="form__input"
+              autoComplete="off"
+              placeholder=" "
+            />
+            <label for="confpassword" class="form__label">
+              Confirmé le mot de passe:
+            </label>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
